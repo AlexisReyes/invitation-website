7ab01ff47ecd2976d757d811a85df3b417052e2c
Alexis Reyes <alexisreyes88@gmail.com>
2022-10-20 00:59:20 -0600

    Fixed Layout

--- website/src/components/content/content.js
+++ website/src/components/content/content.js
@@ -19,32 +19,32 @@ const links = {
 const Content = () => (
 
   <div className="flex flex-col">
-    <div className="flex flex-col lg:mx-32 lg:px-16">
+    <div className="flex flex-col md:mx-32 md:px-16">
       <div className="grow flex flex-col items-center justify-center">
-        <span className="text-7xl font-brown-sunflower text-golden my-4">Domingo</span>
-        <DateEvent className="lg:w-3/4 my-4"/>
-        <Separator className="lg:w-3/4 my-4"/>
+        <span className="md:text-7xl text-5xl font-brown-sunflower text-golden my-4">Domingo</span>
+        <DateEvent className="my-4 sm:w-3/4 w-[300px]"/>
+        <Separator className="my-4"/>
       </div>
       <div className="my-16">
         <div className="flex flex-col items-center justify-center">
-          <span className="text-7xl font-brown-sunflower text-golden">Misa</span>
+          <span className="md:text-7xl text-5xl font-brown-sunflower text-golden">Misa</span>
           <Church className="my-2" />
-          <div className="flex flex-col items-center justify-center font-montserrat-regular text-xl">
-            <span>Parroquia de Nuestra Señora del Rosario</span>
-            <span>Domingo 20 de Noviembre a las 3:00 PM</span>
-            <span className="text-lg">Antonio Norzagaray SN, Centro, 81000 Guasave, Sin.</span>
+          <div className="flex flex-col items-center justify-center font-montserrat-regular md:text-xl text-lg">
+            <span className="text-center">Parroquia de Nuestra Señora del Rosario</span>
+            <span className="text-center">Domingo 20 de Noviembre a las 3:00 PM</span>
+            <span className="md:text-lg text-sm text-center">Antonio Norzagaray SN, Centro, 81000 Guasave, Sin.</span>
             <a className="bg-gray mt-2 p-2 text-white rounded-none hover:rounded" target="_blank" href={links.ceremonyLocation}>Ubicación</a>
           </div>
         </div>
       </div>
       <div className="my-16">
         <div className="flex flex-col items-center justify-center">
-          <span className="text-7xl font-brown-sunflower text-golden">Recepción</span>
+          <span className="md:text-7xl text-5xl font-brown-sunflower text-golden">Recepción</span>
           <GlassOfWine className="my-2" />
-          <div className="flex flex-col items-center justify-center font-montserrat-regular text-xl">
-            <span>Real del Alva</span>
-            <span>Domingo 20 de Noviembre a las 5:00 PM</span>
-            <span className="text-lg">Justo Sierra 335, Col. Ejidal, 81020 Guasave, Sin.</span>
+          <div className="flex flex-col items-center justify-center font-montserrat-regular md:text-xl text-lg">
+            <span className="text-center">Real del Alva</span>
+            <span className="text-center">Domingo 20 de Noviembre a las 5:00 PM</span>
+            <span className="md:text-lg text-sm text-center">Justo Sierra 335, Col. Ejidal, 81020 Guasave, Sin.</span>
             <a className="bg-gray mt-2 p-2 text-white rounded-none hover:rounded" target="_blank" href={links.partyLocation}>Ubicación</a>
           </div>
         </div>
@@ -53,10 +53,10 @@ const Content = () => (
     <div className="flex flex-col items-center justify-center inline">
       {/*<Leaves style={{ height: 350, display: "block"}} />*/}
     </div>
-    <div className="flex flex-col mx-32">
+    <div className="flex flex-col md:mx-32">
       <div className="flex flex-col items-center justify-center my-8">
-        <span className="text-7xl items-center justify-center font-brown-sunflower text-golden">RSVP</span>
-        <div className="flex flex-col items-center justify-center font-montserrat-regular text-xl">
+        <span className="md:text-7xl text-5xl items-center justify-center font-brown-sunflower text-golden">RSVP</span>
+        <div className="flex flex-col items-center justify-center font-montserrat-regular md:text-xl text-lg">
           <span className="text-center">Nos encantaría compartir este día contigo.</span>
           <span className="text-center">Por favor ayudanos a confirmar tu asistencia.</span>
           <a className="bg-gray my-2 p-2 text-white rounded-none hover:rounded" target="_blank" href={links.confirmMessage}>Confirmar</a>
@@ -64,8 +64,8 @@ const Content = () => (
         </div>
       </div>
       <div className="flex flex-col items-center justify-center my-8">
-        <span className="text-7xl font-brown-sunflower text-golden">Regalos</span>
-        <div className="flex flex-col items-center justify-center font-montserrat-regular text-xl">
+        <span className="md:text-7xl text-5xl font-brown-sunflower text-golden">Regalos</span>
+        <div className="flex flex-col items-center justify-center font-montserrat-regular md:text-xl text-lg">
           <span className="text-center">El mejor regalo es tu presencia, pero si quieres obsequiarle algo a nuestra pequeña te damos algunas opciones.</span>
           <Present className="my-2"/>
           <p>Liverpool: 51047977</p>
